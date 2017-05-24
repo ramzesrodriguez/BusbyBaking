93c70618bb072e3b72b766a32e447d79ed7e28a0
Steve Mason <steve@androidbox.me>
2017-05-24 22:06:04 +0700

    Initial commit

---
+++ app/src/main/java/me/androidbox/busbybaking/recipieslist/MainActivity.java
@@ -0,0 +1,15 @@
+package me.androidbox.busbybaking.recipieslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.androidbox.busbybaking.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
