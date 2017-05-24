ad9e7581927f44916c9851beafd3e179acbef969
Steve Mason <steve@androidbox.me>
2017-05-24 22:45:09 +0700

    Added model classes

---
+++ app/src/main/java/me/androidbox/busbybaking/model/Ingredients.java
@@ -0,0 +1,23 @@
+package me.androidbox.busbybaking.model;
+
+/**
+ * Created by steve on 5/24/17.
+ */
+
+public class Ingredients {
+    private int quantity;
+    private String measure;
+    private String ingredient;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+}
---
+++ app/src/main/java/me/androidbox/busbybaking/model/Recipe.java
@@ -0,0 +1,40 @@
+package me.androidbox.busbybaking.model;
+
+import java.util.List;
+
+/**
+ * Created by steve on 5/24/17.
+ */
+
+public class Recipe {
+    private int id;
+    private String name;
+    private List<Ingredients> ingredients;
+    private List<Steps> steps;
+    private int servings;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public List<Steps> getSteps() {
+        return steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
