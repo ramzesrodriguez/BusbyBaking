e35b314da965eac3c16325a274cb91334feed123
Steve Mason <steve@androidbox.me>
2017-05-26 00:13:19 +0700

    Added recipesAPI

--- app/src/main/java/me/androidbox/busbybaking/network/RecipesAPI.java
+++ app/src/main/java/me/androidbox/busbybaking/network/RecipesAPI.java
@@ -1,9 +1,15 @@
 package me.androidbox.busbybaking.network;
 
+import me.androidbox.busbybaking.model.Recipe;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+
 /**
  * Created by steve on 5/25/17.
  */
 
-public class RecipesAPI {
-
+public interface RecipesAPI {
+    @GET
+    Call<Recipe> getReceipe(@Body Recipe recipe);
 }
