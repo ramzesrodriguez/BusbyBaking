80548f2a636de4cdf7ef1e0e01a7f70dd7820a28
Steve Mason <steve@androidbox.me>
2017-05-27 20:57:12 +0700

    Added test

---
+++ app/src/test/java/me/androidbox/busbybaking/recipieslist/RecipeListModuleImpTest.java
@@ -0,0 +1,31 @@
+package me.androidbox.busbybaking.recipieslist;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by steve on 5/27/17.
+ */
+public class RecipeListModuleImpTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getRecipesFromAPI() throws Exception {
+    }
+
+    @Test
+    public void startup() throws Exception {
+    }
+
+    @Test
+    public void shutdown() throws Exception {
+    }
+
+}
