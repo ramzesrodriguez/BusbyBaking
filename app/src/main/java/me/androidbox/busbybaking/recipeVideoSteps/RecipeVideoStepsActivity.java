8f3a2dbdfa59e3fb701689881b48941d75dfd41b
Steve Mason <steve.mason@agoda.com>
2017-06-12 15:28:28 +0700

    Added activity and fragment for the video steps

---
+++ app/src/main/java/me/androidbox/busbybaking/recipeVideoSteps/RecipeVideoStepsActivity.java
@@ -0,0 +1,16 @@
+package me.androidbox.busbybaking.recipeVideoSteps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.androidbox.busbybaking.R;
+
+
+public class RecipeVideoStepsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_video_steps);
+    }
+}
