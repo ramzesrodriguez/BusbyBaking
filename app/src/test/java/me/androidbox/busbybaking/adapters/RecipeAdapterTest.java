15d0f1bcac58af0712c8766f552190ecc29ae223
Steve <stevekitkat@gmail.com>
2017-09-15 18:57:47 +0700

    Update RecipeAdapterTest.java

--- app/src/test/java/me/androidbox/busbybaking/adapters/RecipeAdapterTest.java
+++ app/src/test/java/me/androidbox/busbybaking/adapters/RecipeAdapterTest.java
@@ -72,4 +72,97 @@ public class RecipeAdapterTest extends BaseRobolectricTestRunner {
 
         return recipeList;
     }
+}public class TravelerChatAdapterTest extends BaseAppRobolectricTest {
+
+    private static final String HOST_ID = "1";
+    private static final String CUSTOMER_ID = "2";
+    private static final ZoneId TIME_ZONE = ZoneId.of("Asia/Bangkok");
+
+    private static final int TYPE_INCOMING_MESSAGE = 1;
+    private static final int TYPE_SPECIAL_REQUESTS = 5;
+
+    @Mock ChatAdapter.Controller controller;
+
+    private ChatAdapter adapter;
+    private Context context;
+
+    @Before
+    public void setUp() {
+
+        context = ShadowApplication.getInstance().getApplicationContext();
+
+        MatrixCursor cursor = createMockCursor();
+        adapter = new TravelerChatAdapter(controller, cursor, CUSTOMER_ID);
+        adapter.setCursor(cursor);
+        adapter.setHasMoreData(true);
+        adapter.setChatViewModel(mockChatViewModel());
+
+        Locale.setDefault(Locale.US);
+
+        ClocksMockAccessor.setMockTimeZone(TIME_ZONE);
+    }
+
+    @Test
+    public void onBindIncomingMessageViewHolder() {
+        RecyclerView recyclerView = createRecyclerView();
+        IncomingMessageViewHolder holder = (IncomingMessageViewHolder) adapter.onCreateViewHolder(recyclerView, TYPE_INCOMING_MESSAGE);
+
+        holder.avatarView = spy(holder.avatarView);
+        adapter.onBindViewHolder(holder, 0);
+
+        assertThat(holder.contentTextView.getText(), is("Message 1"));
+        assertThat(holder.timeTextView.getText(), is("00:00"));
+        verify(holder.avatarView).setAvatarImage(anyInt());
+    }
+
+    @Test
+    public void testSpecialRequest_isPopulatedWithValidData() {
+        final SpecialRequestsViewHolder specialRequestsViewHolder = Mockito.mock(SpecialRequestsViewHolder.class);
+
+        adapter.onBindViewHolder(specialRequestsViewHolder, 4);
+
+        verify(specialRequestsViewHolder).populateSpecialRequestContent("Nonsmoking, largeroom", "00:00");
+    }
+
+    @Nonnull private RecyclerView createRecyclerView() {
+        RecyclerView recyclerView = new RecyclerView(context);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        return recyclerView;
+    }
+
+    @Nonnull private MatrixCursor createMockCursor() {
+        MatrixCursor cursor = new MatrixCursor(new String[] {
+                MessageContract.COLUMN_PARTICIPANT_ID,
+                UserContract.COLUMN_NAME,
+                MessageContract.COLUMN_CONTENT,
+                MessageContract.COLUMN_CREATED_TIMESTAMP_UTC,
+                MessageContract.COLUMN_CONTENT_TYPE,
+        });
+        cursor.addRow(createMessageValues(HOST_ID, "Host", "Message 1", DateTimeConverter.toUtcMillis(dateTime(2015, 1, 3)), MessageContentType.TEXT.getIntValue()));
+        cursor.addRow(createMessageValues(CUSTOMER_ID, "Guest", "Message 2", DateTimeConverter.toUtcMillis(dateTime(2015, 1, 2)), MessageContentType.TEXT.getIntValue()));
+        cursor.addRow(createMessageValues(CUSTOMER_ID, "Guest", "Message 3", DateTimeConverter.toUtcMillis(dateTime(2015, 1, 1)), MessageContentType.TEXT.getIntValue()));
+        cursor.addRow(createMessageValues(CUSTOMER_ID, "Guest", "Status Message", DateTimeConverter.toUtcMillis(dateTime(2015, 1, 1)), MessageContentType.STATUS_MESSAGE.getIntValue()));
+        cursor.addRow(createMessageValues(CUSTOMER_ID, "Guest", "Nonsmoking, largeroom", DateTimeConverter.toUtcMillis(dateTime(2017, 9, 15)), MessageContentType.SPECIAL_REQUEST.getIntValue()));
+        return cursor;
+    }
+
+    @Nonnull private OffsetDateTime dateTime(int year, int month, int dayOfMonth) {
+        return ZonedDateTime.of(year, month, dayOfMonth, 0, 0, 0, 0, TIME_ZONE).toOffsetDateTime();
+    }
+
+    @Nonnull
+    private Object[] createMessageValues(String participantId, String participantName, String content, long timestamp, int messageType) {
+        return new Object[] {participantId, participantName, content, timestamp, messageType};
+    }
+
+    private ChatViewModel mockChatViewModel() {
+        PropertyInfo propertyInfo = new PropertyInfo();
+        propertyInfo.avatarType = 1;
+
+        ChatViewModel chatViewModel = new ChatViewModel();
+        chatViewModel.propertyInfo = propertyInfo;
+
+        return chatViewModel;
+    }
 }
+
